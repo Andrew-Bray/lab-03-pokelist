578a67b02156f9c11e3b347b178bfa6e1f093362
Andrew-Bray <amichaelbray@gmail.com>
2020-10-29 15:13:50 -0700

    new router wrok added

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
             <header>
                 <h1>Who's your Favorite Pokemon?</h1>
+                <Link to="/">Home</Link>
+                <Link to="/list">Pokemons!!</Link>
             </header>
         )
     }
 }
+// Finish linking
