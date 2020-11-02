a3e631b5533cb38dbefdf84b7b7fb0bc1fb0f24b
Andrew-Bray <amichaelbray@gmail.com>
2020-11-02 08:06:04 -0800

    add paginate link

--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,7 @@ export default class Header extends Component {
                 <h1>Who's your Favorite Pokemon?</h1>
                 <Link to="/">Home</Link>
                 <Link to="/list">Pokemons!!</Link>
+                <Link to="/paginate">Paginated Pokemons!!</Link>
             </header>
         )
     }
